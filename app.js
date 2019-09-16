f9c03786d18033e55894adce6c90dd23ff8cbc64
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 13:58:04 -0600

    function drills 1 final

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 function getYearOfBirth (age) {
-  if (age < 0 ){
-    throw new Error ('Age can not be negative');
-  } 
   return 2019 - age;
 }
 
@@ -11,8 +8,12 @@ function createGreeting(name, age) {
   if (name === undefined || age === undefined) {
     throw new Error ('Arguments not valid');
   }
+  if (age < 0 ){
+    throw new Error ('Age can not be negative');
+  } 
+
   if (typeof name !== 'string' || typeof age !== 'number'){
-    throw new TypeError('invalid entry', 'app.js');
+    throw new TypeError('Invalid Entry');
   }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
@@ -20,7 +21,7 @@ function createGreeting(name, age) {
 
 
 try {
-  const greeting1 = createGreeting('Shannon', 27);
+  const greeting1 = createGreeting(27, 27);
   console.log(greeting1);
 } catch(err) {
   console.log(err);
