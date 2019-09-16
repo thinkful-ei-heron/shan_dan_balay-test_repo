45f5484338db717c07005e8bcfba49e0b933746f
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 13:25:13 -0600

    create const

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting() {
-    console.log('Hi my name is Shannon and I\'m 27 years old');
+    return 'Hi my name is Shannon and I\'m 27 years old';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
