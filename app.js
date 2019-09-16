ba81d59fd895aa5369887f43c9aedb6d701d9806
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 13:46:35 -0600

    valid arguments error

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 'use strict';
 
 function getYearOfBirth (age) {
-    if (age < 0 ){
-        throw new Error ("Age can not be negative");
-    } 
-    return 2019 - age;
+  if (age < 0 ){
+    throw new Error ('Age can not be negative');
+  } 
+  return 2019 - age;
 }
 
 function createGreeting(name, age) {
-    const yearOfBirth = getYearOfBirth(age);
-    return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
+  if (name === undefined || age === undefined) {
+    throw new Error ('Arguments not valid');
+  }
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 
 try {
-const greeting1 = createGreeting('Shannon', -10);
-console.log(greeting1);
+  const greeting1 = createGreeting('Shannon');
+  console.log(greeting1);
 } catch(err) {
-    console.log(err);
+  console.log(err);
 }
