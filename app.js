f90c2c40fd2410dd4a308e0a98844fc13b38c3e0
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 13:54:38 -0600

    typeError

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ function createGreeting(name, age) {
   if (name === undefined || age === undefined) {
     throw new Error ('Arguments not valid');
   }
+  if (typeof name !== 'string' || typeof age !== 'number'){
+    throw new TypeError('invalid entry', 'app.js');
+  }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 
 try {
-  const greeting1 = createGreeting('Shannon');
+  const greeting1 = createGreeting('Shannon', 27);
   console.log(greeting1);
 } catch(err) {
   console.log(err);
