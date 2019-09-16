4c4037cfd63618a817307ebcee43b91d6384e868
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-16 13:33:51 -0600

    getYearOfBirth function

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 'use strict';
 
+function getYearOfBirth (age) {
+    return 2019 - age;
+}
+
 function createGreeting(name, age) {
-    const yearOfBirth = 2019 - age;
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
+
+
 
 const greeting1 = createGreeting('Shannon', 27);
 console.log(greeting1);
